7612673194f90f6cf49763c4635a71a8a2e3f07c
Tina12Jumde <158752623+Tina12Jumde@users.noreply.github.com>
2024-06-26 12:56:08 +0530

    emoji

--- src/components/sections/EmojiGrid.jsx
+++ src/components/sections/EmojiGrid.jsx
@@ -1,25 +1,51 @@
-import React from 'react'
-
-import {emojies} from "../sections/Data.js"
-
-let displayEmojies = (props) => {
-    return (
-        <div key={props.id} className='col-2 p-2 shadow-lg'>
-            <div className='row align-items-center'>
-                <div className='col-4'>
-                    <h1>{props.icon}</h1>
-                </div>
-                <div className='col'>
-                    <span className='fw-bold fs-5'>{props.name}</span>
-                </div>
-            </div>
-            <p>{props.discription}</p>
-        </div>
-    )
-}
+import React, { useState } from 'react'
 
 const EmojiGrid = (props) => {
 
+    let [isOpen, setIsOpen] = useState(false)
+
+
+    let displayEmojies = (props) => {
+        return (
+            <div key={props.id} className='col-2 p-2 shadow-lg'>
+                <div className='row align-items-center'>
+                    <div className='col-4'>
+                        <h1>{props.icon}</h1>
+                    </div>
+                    <div className='col'>
+                        <span className='fw-bold fs-5'>{props.name}</span>
+                    </div>
+                </div>
+                <p>{props.discription.slice(0, 22) + "..."}</p>
+
+                <button className='btn btn-sm btn-primary' onClick={() => { setIsOpen(true) }}>view</button>
+
+                {isOpen ? <EmojiPopUp emoji={props} /> : null}
+
+            </div>
+        )
+    }
+
+    let EmojiPopUp = (props) => {
+        return (
+            <div className={isOpen ? "" : "collapse"} id='emoji-pop-up'>
+                <div style={{ width: "600px", height: "600px" }} className='bg-dark position-fixed z-3 start-50 top-50 translate-middle d-flex flex-column justify-content-center align-items-center'>
+                    <div style={{ fontSize: "10rem" }} className=''>
+                        {props.emoji.icon}
+                    </div>
+                    <div>
+                        <h3 className='text-light '>{props.emoji.name}</h3>
+                    </div>
+                    <div className="text-light">
+                        <p className='fs-8 p-5'>{props.emoji.discription}</p>
+                    </div>
+                    <button className='btn bg-danger btn-close' onClick={() => {
+                        setIsOpen(false)
+                    }}></button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <>
@@ -27,7 +53,7 @@ const EmojiGrid = (props) => {
                 <div className="container">
                     <div className="row gap-3 justify-content-center py-5">
 
-                      
+
                         {/* display all emojies from the array of emojies */}
                         {
                             props.data.map(displayEmojies)
@@ -41,3 +67,4 @@ const EmojiGrid = (props) => {
 }
 
 export default EmojiGrid
+
